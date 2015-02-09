270368a699806371d6afcc4b6a3b82210e38466a
lfernando.orozco <lfernando.orozco@localhost>
2015-02-09 14:47:28 -0500

    agrego clase persona

---
+++ src/java/com/co/Persona.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.co;
+
+/**
+ *
+ * @author lfernando.orozco
+ */
+public class Persona {
+    private String nombre;
+    private int edad;
+}
